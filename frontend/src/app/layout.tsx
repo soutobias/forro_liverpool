59de91a6b925a72d6d4aa337dc3759b1508ebb41
tobfer <soutobias@yahoo.com.br>
2024-01-17 19:27:41 +0000

    add

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,6 +12,8 @@ import { ReactNode } from 'react'
 import localFont from '@next/font/local'
 // import { NavBar } from '@/components/NavBar'
 import { LanguageProvider } from '@/lib/language'
+import Script from 'next/script'
+import forroLogoInstagram from '@/assets/fl_logo_instagram.png'
 
 const pieches = localFont({
   src: [
@@ -34,6 +36,24 @@ const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans' })
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Forro Liverpool</title>
+        <link rel="icon" type="image/png" href="/images/fl_logo.png" />
+        <meta
+          name="description"
+          content="Brazilian forró classes & events in the heart of Liverpool"
+        />
+        <meta property="og:title" content="Forro Liverpool" />
+        <meta property="og:url" content="https://www.forroliverpool.co.uk/" />
+        <meta
+          property="og:description"
+          content="Brazilian forró classes & events in the heart of Liverpool"
+        />
+        <meta property="og:image" content="/images/meta_image1.png" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en-GB" />
+      </head>
       <body
         className={`${pieches.variable} ${roboto.variable} ${inter.variable} ${changa.variable} ${openSans.variable}  main-page font-sans`}
       >
