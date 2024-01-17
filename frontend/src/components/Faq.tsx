5665fef6e95c0a66c9db5d4e4f513e7e78669e71
VanessaBach <vanessabach.r@gmail.com>
2024-01-17 16:38:00 +0000

    fix

--- frontend/src/components/Faq.tsx
+++ frontend/src/components/Faq.tsx
@@ -15,7 +15,130 @@ interface FaqProps {
   question?: any
 }
 
-export function Faq(props: { isFestival: boolean }) {
+const ExternalLink = ({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) => (
+  <a
+    style={{
+      textDecoration: 'underline',
+      display: 'inline-flex',
+      alignItems: 'center',
+    }}
+    href={href}
+    target="_blank"
+    rel="noreferrer"
+  >
+    {children}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="currentColor"
+      viewBox="0 0 256 256"
+      style={{ verticalAlign: 'middle' }}
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <line
+        x1="64"
+        y1="192"
+        x2="192"
+        y2="64"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      ></line>
+      <polyline
+        points="88 64 192 64 192 168"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      ></polyline>
+    </svg>
+  </a>
+)
+
+export const FaqQuestion = ({ question, answer }: FaqProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleToggleQuestion = () => {
+    setIsOpen(!isOpen)
+  }
+
+  // Função para substituir o texto por links
+  const replaceTextWithLinks = (text: string) => {
+    const parts = text.split(
+      /(Forró Liverpool\.|trainline\.com|nationalexpress\.co\.uk|hello@forroliverpool\.co\.uk|@forro\.liverpool)/g,
+    )
+
+    return parts.map((part, index) => {
+      if (index % 2 === 1) {
+        const links: Record<string, string> = {
+          'Forró Liverpool.':
+            'https://open.spotify.com/playlist/6Luuh8cKFXtEY8t2MIL2P4?si=EEAc2-uQQaarFD1_m5MsjA&pi=e-qxmnaQS7QNSJ&nd=1&dlsi=6f2d3024fc934803',
+          'thetrainline.com': 'http://trainline.com/',
+          'nationalexpress.co.uk': 'https://www.nationalexpress.com/en',
+          'hello@forroliverpool.co.uk': 'mailto:hello@forroliverpool.co.uk',
+          '@forro.liverpool':
+            'https://www.instagram.com/forro.liverpool/?igsh=MW5sMmlhaXp4Zms2aA%3D%3D',
+        }
+
+        return (
+          <ExternalLink key={index} href={links[part]}>
+            {part}
+          </ExternalLink>
+        )
+      } else {
+        return part
+      }
+    })
+  }
+
+  return (
+    <div className="pt-4">
+      <div className="border-b-2 pb-4 border-b-gray-100">
+        <div
+          className="flex justify-between items-center cursor-pointer"
+          onClick={handleToggleQuestion}
+        >
+          <div className="flex justify-left w-10/12 items-center">
+            <div>
+              {/* Você pode precisar importar SpeechBubbles */}
+              <SpeechBubbles />
+            </div>
+            <div>
+              <p className="pl-4 text-[1.25rem] md:text-[1.5rem] leading-6 md:leading-8 font-extrabold">
+                {question}
+              </p>
+            </div>
+          </div>
+          <FontAwesomeIcon
+            icon={isOpen ? faChevronDown : faChevronLeft}
+            className="text-xl"
+          />
+        </div>
+        {isOpen && (
+          <div className="pt-4 flex">
+            <div>
+              <div className="text-[1rem] leading-6 md:text-[1.25rem] md:leading-7 font-semibold font-sans">
+                {replaceTextWithLinks(answer)}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export const Faq = (props: { isFestival: boolean }) => {
   const { isFestival } = props
   const [questions, setQuestions] = useState<keyable[] | null>(null)
   const { language } = useLanguage()
@@ -61,84 +184,6 @@ export function Faq(props: { isFestival: boolean }) {
             answer={item.answer}
           />
         ))}
-    </div>
-  )
-}
-
-export function FaqQuestion({ question, answer }: FaqProps) {
-  const [isOpen, setIsOpen] = useState(false)
-
-  function handleToggleQuestion() {
-    setIsOpen(!isOpen)
-  }
-
-  // Função para substituir o texto por links
-  const replaceTextWithLinks = (text: string) => {
-    const replacedText = text
-      .replace(
-        'Forró Liverpool.',
-        `<a style="text-decoration: underline; display: inline-flex; align-items: center;" href="https://open.spotify.com/playlist/6Luuh8cKFXtEY8t2MIL2P4?si=EEAc2-uQQaarFD1_m5MsjA&pi=e-qxmnaQS7QNSJ&nd=1&dlsi=6f2d3024fc934803" target="_blank">
-          <span style="margin-right: 4px;">Forró Liverpool</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256" style="vertical-align: middle;">
-            <rect width="256" height="256" fill="none"></rect>
-            <line x1="64" y1="192" x2="192" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-            <polyline points="88 64 192 64 192 168" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline>
-          </svg>
-        </a>`,
-      )
-      .replace(
-        /thetrainline\.com/g,
-        '<a style="text-decoration: underline; display: inline-flex; align-items: center;" href="http://trainline.com/" target="_blank">trainline.com<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256" style="vertical-align: middle;"><rect width="256" height="256" fill="none"></rect><line x1="64" y1="192" x2="192" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><polyline points="88 64 192 64 192 168" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg></a>',
-      )
-      .replace(
-        /nationalexpress\.co\.uk/g,
-        '<a style="text-decoration: underline; display: inline-flex; align-items: center;" href="https://www.nationalexpress.com/en" target="_blank">nationalexpress.co.uk<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256" style="vertical-align: middle;"><rect width="256" height="256" fill="none"></rect><line x1="64" y1="192" x2="192" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><polyline points="88 64 192 64 192 168" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg></a>',
-      )
-      .replace(
-        /hello@forroliverpool\.co\.uk/g,
-        '<a style="text-decoration: underline; display: inline-flex; align-items: center;" href="mailto:hello@forroliverpool.co.uk">hello@forroliverpool.co.uk<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256" style="vertical-align: middle;"><rect width="256" height="256" fill="none"></rect><line x1="64" y1="192" x2="192" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><polyline points="88 64 192 64 192 168" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg></a>',
-      )
-      .replace(
-        /@forro\.liverpool/g,
-        '<a style="text-decoration: underline; display: inline-flex; align-items: center;" href="https://www.instagram.com/forroliverpool/" target="_blank">@forro.liverpool<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256" style="vertical-align: middle;"><rect width="256" height="256" fill="none"></rect><line x1="64" y1="192" x2="192" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><polyline points="88 64 192 64 192 168" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg></a>',
-      )
-
-    return { __html: replacedText }
-  }
-
-  return (
-    <div className="pt-4">
-      <div className="border-b-2 pb-4 border-b-gray-100">
-        <div
-          className="flex justify-between items-center cursor-pointer"
-          onClick={handleToggleQuestion}
-        >
-          <div className="flex justify-left w-10/12 items-center">
-            <div>
-              <SpeechBubbles />
-            </div>
-            <div>
-              <p className="pl-4 text-[1.25rem] md:text-[1.5rem] leading-6 md:leading-8 font-extrabold">
-                {question}
-              </p>
-            </div>
-          </div>
-          <FontAwesomeIcon
-            icon={isOpen ? faChevronDown : faChevronLeft}
-            className="text-xl"
-          />
-        </div>
-        {isOpen && (
-          <div className="pt-4 flex">
-            <div>
-              <div
-                className="text-[1rem] leading-6 md:text-[1.25rem] md:leading-7 font-semibold font-sans"
-                dangerouslySetInnerHTML={replaceTextWithLinks(answer)}
-              />
-            </div>
-          </div>
-        )}
-      </div>
     </div>
   )
 }
