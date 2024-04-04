f1f7bc474c6cd53372ff8aa49ccaf8b052cc4cae
tobfer <soutobias@yahoo.com.br>
2024-04-04 20:52:32 +0100

    deploy render

--- config/puma.rb
+++ config/puma.rb
@@ -19,7 +19,7 @@ if ENV['RAILS_ENV'] == 'production'
   worker_count = Integer(ENV.fetch('WEB_CONCURRENCY') { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
 end
-
+preload_app!
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
